09beba0503a956e1e7b3db718f2975d3ad82d037
Arnab048 <codingjedi048@gmail.com>
2023-08-21 17:06:26 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+
+    fontFamily: {
+      'raleway':'Raleway'
+    },
+    extend: {
+
+      colors: {
+        'red-pigment': '#EB1724',
+      },
+
+    },
+  },
+  plugins: [],
+}
